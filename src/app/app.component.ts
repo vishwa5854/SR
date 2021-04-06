12d5b1dc073a346fe439e2732e72f3a23fd35b7c
Vishwanath <vishwanath@freshprints.com>
2021-04-06 15:43:10 +0530

    vishwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { StripeService, Elements, Element as StripeElement, ElementsOptions } from 'ngx-stripe';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SR';
+  stripeForm: FormGroup;
+  elements: Elements;
+  card: StripeElement;
+  paymentStatus:any ;
+  stripeData: any;
+
+  elementsOptions: ElementsOptions = {
+    locale: 'en'
+  }
+
+  constructor(private formBuilder: FormBuilder, private stripeService: StripeService) {
+
+  }
+
+  ngOnInit() {
+    this.createForm();
+
+    this.stripeService.elements(this.elementsOptions).subscribe(elements => {
+      this.elements = elements;
+      if(!this.card) {
+        this.card = this.elements.create('card', {
+          iconStyle: 'solid',
+          style: {
+            base: {
+              iconColor: '#666EE8',
+              color: '#31325F',
+              lineHeight: '40px',
+              fontWeight: 300,
+              fontFamily: '"Verdana"',
+              fontSize: '18px',
+              '::placeholder': {
+                color: '#CFD7E0'
+              }
+            }
+          }
+        });
+        this.card.mount('#card-element');
+      }
+    })
+  }
+
+  createForm() {
+    this.stripeForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      amount: ['', [Validators.required]]
+    });
+  }
+  public buy() {
+    this.stripeData = this.stripeForm.value;
+    this.stripeService.createToken(this.card, { name: 'hey' }).subscribe(result => {
+      if(result.token) {
+        this.stripeData['token'] = result.token;
+        console.log(this.stripeData);
+        
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxStripeModule } from 'ngx-stripe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxStripeModule.forRoot('pk_test_51I0tBLHjDycGB3bI5KY701EORzL4a4Fwzu7LfhhlzvfDgA2Ifo1rYhutDHaFNvR6i0CZqp0AplSYBQcHSTwXM0CQ00kkW6q5V1')
   ],
   providers: [],
   bootstrap: [AppComponent]
